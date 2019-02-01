d19bc03152d0feb97f2cf93bc275ba9395c816d5
Nikola Siderov <iamnikkysiderov@gmail.com>
2019-02-01 17:18:10 +0300

    started building snake game

---
+++ Snake/SnakeGame/Snake.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGame
+{
+    class Snake
+    {
+        public List<char> Body { get; set; }
+
+        public char Symbol { get; set; }
+        
+        public List<int> BodyCoordinatesX { get; set; }
+
+        public List<int> BodyCoordinatesY { get; set; }
+
+        public Snake()
+        {
+            this.Body = new List<char>();
+            this.BodyCoordinatesX = new List<int>();
+            this.BodyCoordinatesY = new List<int>();
+        }
+    }
+}
---
+++ Snake/SnakeGame/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading;
+
+namespace SnakeGame
+{
+    // spawn fruits
+    // move snake
+    // grow snake
+    // create snake
+
+    class Startup
+    {
+        static bool eaten = false;
+
+        static void Main(string[] args)
+        {
+            Console.BufferHeight = Console.WindowHeight = 40;
+            Console.BufferWidth = Console.WindowWidth = 100;
+
+            Run();
+        }
+
+        static void Run()
+        {
+            while (true)
+            {
+                if (eaten)
+                {
+                    SpawnFruits();
+                }
+
+                MoveSnake();
+                GrowSnake();
+
+                Console.Clear();
+
+                Thread.Sleep(100);
+
+                CreateSnake();
+            }
+        }
+
+        static void SpawnFruits()
+        {
+            Fruit fruit = new Fruit();
+            fruit.Symbol = '@';
+
+            Random randomGen = new Random();
+            int chanceOfX = randomGen.Next(1, 100);
+            int chanceOfY = randomGen.Next(1, 40);
+
+            Console.SetCursorPosition(chanceOfX, chanceOfY);
+            Console.Write(fruit.Symbol);
+        }
+
+        static void MoveSnake()
+        {
+
+        }
+
+        static void GrowSnake()
+        {
+
+        }
+
+        static void CreateSnake()
+        {
+            Snake snake = new Snake();
+            snake.Symbol = 'O';
+            snake.Body.Add(snake.Symbol);
+            snake.BodyCoordinatesX.Add(1);
+            snake.BodyCoordinatesY.Add(1);
+
+            for (int i = 0; i < snake.Body.Count; i++)
+            {
+                Console.SetCursorPosition(snake.BodyCoordinatesX[i], snake.BodyCoordinatesY[i]);
+                Console.Write(snake.Symbol);
+            }
+        }
+    }
+}
